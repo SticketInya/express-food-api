711b9c3f7225e56def160e04e1bb491b1e7483a5
Patrik Jokhel <mr.booman32@gmail.com>
2022-03-14 14:35:55 +0100

    feat(index.ts): setup barebones express server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 //TODO: build your application here
+import express, { NextFunction, Request, Response, Router } from 'express';
+
+const PORT = parseInt(process.env.PORT || '3000', 10);
+
 const main = async (): Promise<void> => {
 //TODO: start your application here
+  const app = express();
+
+  app.get('/', (req, res)=>{
+    return res.send("Hello World!");
+  })
+
+  app.listen(PORT, '0.0.0.0', ()=>{
+    console.log(`Listening on port ${PORT} `);
+  });
 }
 
 main().catch(console.error);
