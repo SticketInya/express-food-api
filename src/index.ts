f807c1a079f03622461e20d45a30fafe9a08780b
Patrik Jokhel <mr.booman32@gmail.com>
2022-03-16 17:46:01 +0100

    refactor(index.ts): code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,44 @@
 //TODO: build your application here
 import express, { NextFunction, Request, Response, Router } from 'express';
-import {FoodEntry, FoodEntryCreateOptions, FoodEntryDetails, FoodEntryUpdateOptions} from './interfaces';
-import {randomUUID, randomBytes} from 'crypto';
-import 'dotenv/config'
+import { FoodEntry, FoodEntryDetails } from './interfaces';
+import { randomUUID, randomBytes } from 'crypto';
+import 'dotenv/config';
 
-const jwt = require('jsonwebtoken');
-const morgan = require('morgan');
+import jwt = require('jsonwebtoken');
+import morgan = require('morgan');
 const PORT = parseInt(process.env.PORT || '3000', 10);
 const privateKey = process.env.PRIVATE_KEY || randomBytes(64).toString();
 
 const main = async (): Promise<void> => {
-//TODO: start your application here
+  //TODO: start your application here
   const app = express();
   const foodRouter = Router();
-  let foods : FoodEntry[] = [];
+  const foods: FoodEntry[] = [];
 
   app.use(express.json());
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(morgan('combined'));
   app.disable('x-powered-by');
 
-  foodRouter.get('', (req, res)=>{
-    if(!foods.length){
+  foodRouter.get('', (req, res) => {
+    if (!foods.length) {
       return res.sendStatus(204);
     }
     return res.status(200).send(foods);
-  })
+  });
 
-  foodRouter.get('/:id', (req, res) =>{
+  foodRouter.get('/:id', (req, res) => {
     const { id: foodId } = req.params;
-    const foodExists = foods.find(food => food.id === foodId);
-    if(!foodExists){
-      return res.status(404).send({error:"food not found"});
+    const foodExists = foods.find((food) => food.id === foodId);
+    if (!foodExists) {
+      return res.status(404).send({ error: 'food not found' });
     }
     return res.status(200).send(foodExists);
-  })
+  });
 
-  foodRouter.post('', (req, res)=>{
-    const { name, details } = req.body as { name:string, details:FoodEntryDetails };
-    const newFoodEntry: FoodEntry ={
+  foodRouter.post('', (req, res) => {
+    const { name, details } = req.body as { name: string; details: FoodEntryDetails };
+    const newFoodEntry: FoodEntry = {
       id: randomUUID().toString(),
       createdAt: new Date(),
       name: name,
@@ -45,69 +46,74 @@ const main = async (): Promise<void> => {
     };
     foods.push(newFoodEntry);
     return res.status(201).send(newFoodEntry);
-  })
+  });
 
-  foodRouter.put('/:id', (req, res)=>{
+  foodRouter.put('/:id', (req, res) => {
     const { id: foodId } = req.params;
-    const foodUpdate = req.body;
-    const foodIndex = foods.findIndex(food=>food.id === foodId);
-    if(foodIndex === -1){
+    const { name: updateName, details: updateDetails } = req.body as { name: string; details: FoodEntryDetails };
+    const foodIndex = foods.findIndex((food) => food.id === foodId);
+    if (foodIndex === -1) {
       return res.status(404).send('food not found');
     }
-    foods[foodIndex].name = foodUpdate.name.toString();
+    foods[foodIndex].name = updateName;
+    foods[foodIndex].details = updateDetails;
     return res.status(200).send(foods[foodIndex]);
-  })
+  });
 
-  foodRouter.delete('/:id', (req, res)=>{
+  foodRouter.delete('/:id', (req, res) => {
     const { id: foodId } = req.params;
-    const foodIndex = foods.findIndex(food=> food.id === foodId);
-    if(foodIndex !== -1){
+    const foodIndex = foods.findIndex((food) => food.id === foodId);
+    if (foodIndex !== -1) {
       foods.splice(foodIndex, 1);
     }
     return res.sendStatus(204);
-  })
+  });
 
-  app.post('/login', (req, res)=>{
+  app.post('/login', (req, res) => {
     const validUsername = process.env.VALID_USERNAME;
     const validPassword = process.env.VALID_PASSWORD;
-    const { username, password } = req.body;
+    const { username, password } = req.body as { username: string; password: string };
+    const areValidCredentials = username === validUsername && password === validPassword;
 
-    if(username===validUsername && password===validPassword){
-      const signedToken = jwt.sign({user:username}, privateKey);
-      return res.status(200).send({token:signedToken});
+    if (areValidCredentials) {
+      const signedToken = jwt.sign({ user: username }, privateKey);
+      return res.status(200).send({ token: signedToken });
     }
-    return res.status(401).send({error:"invalid login credentials"});
-  })
+    return res.status(401).send({ error: 'invalid login credentials' });
+  });
 
   app.use('/food', authenticateToken, foodRouter);
 
-  app.use('*',(req, res)=>{
-    return res.status(404).send({error:'not found'});
-  })
+  app.use('*', (req, res) => {
+    return res.status(404).send({ error: 'not found' });
+  });
 
-  app.use((err:unknown, req:Request, res:Response, _next: NextFunction)=>{
-    return res.status(500).send({error:`internal server error`});
-  })
+  app.use((err: unknown, req: Request, res: Response, _next: NextFunction) => {
+    return res.status(500).send({ error: `internal server error` });
+  });
 
-  app.listen(PORT, '0.0.0.0', ()=>{
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Listening on port ${PORT}...`);
   });
 
-
-  function authenticateToken(req:Request, res:Response, next:NextFunction){
-    const { authorization: authHeader } = req.headers;
-    const token = authHeader && authHeader.split(' ')[1];
-    if(!token){
+  function authenticateToken(req: Request, res: Response, next: NextFunction): unknown {
+    const { authorization } = req.headers;
+    const authHeader = authorization?.split(' ');
+    const isBearer = authHeader && authHeader[0] === 'Bearer';
+    if (!isBearer) {
       return res.sendStatus(401);
     }
-    jwt.verify(token, privateKey, (err:Error)=>{
-      if(err){
+    const token = authHeader[1];
+    if (!token) {
+      return res.sendStatus(401);
+    }
+    jwt.verify(token, privateKey, (err) => {
+      if (err) {
         return res.sendStatus(401);
       }
-    next();
+      next();
     });
   }
-
-}
+};
 
 main().catch(console.error);
